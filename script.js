6fa408044190cd7aa30580633e8cac889045d3fb
Eric Segawa Montagnér <eeric.segawa@gmail.com>
2026-03-04 16:50:11 -0300

    more things done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ import {menuArray} from "./data.js"
 
 const container = document.getElementById("container")
 const container2 = document.getElementById("container2")
+const precoFinal = document.getElementById("preco-final")
+const pedido = document.getElementById("pedido")
 
 function render(arr) {
     return arr.map((item)=>{
@@ -13,13 +15,52 @@ function render(arr) {
                 <h6 class="ingredients">${item.ingredients.join(", ")}</h6>
                 <p class="price">${item.price}$</p>
             </div>
-            <button class="add-btn">+</button>
+            <button class="add-btn" data-btn=${item.id}>+</button>
         </div>
         `
     }).join("")
     
 }
 
+const pedidoArray = []
 
+container.addEventListener("click", function(e){
+    if(e.target.dataset.btn) {
+        container2.style.display = 'flex'
+        addOrder(e.target.dataset.btn)
+        renderOrder()
+    }
+})
+
+function addOrder(id) {
+    
+    const target = menuArray.find((item)=>{
+        return item.id == id
+    })
+
+    if (target) {
+        pedidoArray.push(target)
+    }
+}
+
+function renderOrder() {
+    let total = 0
+
+    const pedidoHtml = pedidoArray.map((item) => {
+        total += item.price
+        return `
+        <div class="pedido-div">
+            <span class="pedido-span">
+                <h4>${item.name}</h4>
+                <button class="remove-btn" data-remove="${item.id}">-</button>
+            </span>
+            <h4>${item.price}$</h4>
+        </div>
+        `
+    }).join("")
+
+    pedido.innerHTML = pedidoHtml
+    precoFinal.textContent = `${total}$`
+}
 
 container.innerHTML = render(menuArray)
